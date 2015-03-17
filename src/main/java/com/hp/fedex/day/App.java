bd38287dcca222d3f03e53609858f09263b76b17
Zhaokun Li <zhao-kun.li@hp.com>
2015-03-17 11:24:40 +0800

    First Commit

---
+++ src/main/java/com/hp/fedex/day/App.java
@@ -0,0 +1,13 @@
+package com.hp.fedex.day;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
